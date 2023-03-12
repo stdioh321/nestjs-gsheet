c33eac75a5733c1ecba5d4689416c12d5ec21bcd
Henrique <henrique.dias@gruponewway.com.br>
2023-03-12 11:45:08 -0300

    chore: replace \\n to \n on GOOGLE_PRIVATE_KEY

--- src/configs/google-sheets-config.service.ts
+++ src/configs/google-sheets-config.service.ts
@@ -4,11 +4,13 @@ import { google, sheets_v4 } from 'googleapis';
 
 @Injectable()
 export class GoogleSheetsConfigService {
-  public constructor(private configService: ConfigService) { }
+  public constructor(private configService: ConfigService) {}
 
   public async getSheets(): Promise<sheets_v4.Sheets> {
     const credentials = {
-      private_key: this.configService.get('google.privateKey'),
+      private_key: this.configService
+        .get('google.privateKey')
+        .replace(/\\n/g, '\n'),
       client_email: this.configService.get('google.clientEmail'),
       client_id: this.configService.get('google.clientId'),
     };
