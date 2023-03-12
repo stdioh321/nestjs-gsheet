5c7f3671d89b638d86c776a340a327bc7116f0f1
Henrique <henrique.dias@gruponewway.com.br>
2023-03-12 17:00:16 -0300

    chore: update status code when add row

--- src/google-sheets/google-sheets.controller.ts
+++ src/google-sheets/google-sheets.controller.ts
@@ -158,7 +158,6 @@ export class GoogleSheetsController {
   })
   @ApiOperation({ summary: 'Add row' })
   @Post('')
-  @HttpCode(200)
   async addRow(@Query() sheetParams: SheetParamsForm, @Body() body) {
     const { projectId, sheet } = sheetParams;
     return await this.googleSheetsService.addRow(projectId, sheet, body);
