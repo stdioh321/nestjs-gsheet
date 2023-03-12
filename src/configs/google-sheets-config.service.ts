5dd312c9b21662fd65d2c24b299de316e8eae217
Henrique <henrique.dias@gruponewway.com.br>
2023-03-12 11:53:06 -0300

    chore: logging credentials

--- src/configs/google-sheets-config.service.ts
+++ src/configs/google-sheets-config.service.ts
@@ -4,16 +4,20 @@ import { google, sheets_v4 } from 'googleapis';
 
 @Injectable()
 export class GoogleSheetsConfigService {
-  public constructor(private configService: ConfigService) {}
+  public constructor(private configService: ConfigService) { }
 
   public async getSheets(): Promise<sheets_v4.Sheets> {
     const credentials = {
       private_key: this.configService
-        .get('google.privateKey')
+        .get<string>('google.privateKey')
         .replace(/\\n/g, '\n'),
       client_email: this.configService.get('google.clientEmail'),
       client_id: this.configService.get('google.clientId'),
     };
+
+    console.log(credentials);
+    console.log(JSON.stringify(credentials));
+
     const auth = await google.auth.getClient({
       scopes: ['https://www.googleapis.com/auth/spreadsheets'],
       // keyFile: 'google_credentials.json',
